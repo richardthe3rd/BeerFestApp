b97fbda329bf4dc9747bcc2fc6930efe328dcb81
Richard Alcock <richard.alcock@gmail.com>
2012-02-02 21:18:27 +0000

    Use JSON format that has array of producers contain array of products.

--- src/ralcock/cbf/model/JsonBeerList.java
+++ src/ralcock/cbf/model/JsonBeerList.java
@@ -10,18 +10,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Vector;
 
 public class JsonBeerList implements Iterable<Beer> {
 
     private static final String TAG = JsonBeerList.class.getName();
 
-    private static final String JSON_BREWERY = "brewery";
-    private static final String JSON_NAME = "name";
-    private static final String JSON_NOTES = "notes";
-    private static final String JSON_ABV = "abv";
+    private static final String PRODUCERS = "producers";
+    private static final String NAME = "name";
+    private static final String NOTES = "notes";
+    private static final String PRODUCE = "produce";
+    private static final String ABV = "abv";
 
     private final InputStream fInputStream;
-    private JSONArray fJsonArray;
+    private List<Beer> fBeerList;
 
     public JsonBeerList(final InputStream inputStream) throws IOException, JSONException {
         assert inputStream!=null;
@@ -30,10 +33,11 @@ public class JsonBeerList implements Iterable<Beer> {
 
     public Iterator<Beer> iterator() {
         try {
-            if (fJsonArray == null) {
-                fJsonArray = loadJson(fInputStream);
+            if (fBeerList == null) {
+                JSONObject jsonObject = loadJson(fInputStream);
+                fBeerList = makeBeerList(jsonObject);
             }
-            return new JSONArrayBeerIterator(fJsonArray);
+            return fBeerList.iterator();
         } catch (JSONException jsx) {
             throw new RuntimeException(jsx);
         } catch (IOException e) {
@@ -41,58 +45,29 @@ public class JsonBeerList implements Iterable<Beer> {
         }
     }
 
-    private static class JSONArrayBeerIterator implements Iterator<Beer>{
-
-        private final JSONArray fJsonArray;
-        private int index = 0;
-
-        JSONArrayBeerIterator(final JSONArray jsonArray) throws IOException, JSONException {
-            fJsonArray = jsonArray;
-        }
-
-        public boolean hasNext() {
-            return index < fJsonArray.length();
-        }
-
-        public Beer next() {
-            try {
-                JSONObject jsonObject = fJsonArray.getJSONObject(index++);
-                JSONObject jsonBreweryObject = jsonObject.getJSONObject(JSON_BREWERY);
-                Beer beer = createBeer(
-                        jsonBreweryObject.getString(JSON_NAME),
-                        jsonBreweryObject.getString(JSON_NOTES),
-                        jsonObject.getString(JSON_NAME),
-                        jsonObject.getString(JSON_ABV),
-                        jsonObject.getString(JSON_NOTES)
-                );
-                return beer;
-            } catch (JSONException jsx) {
-                throw new RuntimeException(jsx);
+    private List<Beer> makeBeerList(final JSONObject json) throws JSONException {
+        Vector<Beer> beers = new Vector<Beer>();
+        JSONArray producers = json.getJSONArray(PRODUCERS);
+        for(int producerIndex=0; producerIndex<producers.length(); producerIndex++){
+            JSONObject producer = producers.getJSONObject(producerIndex);
+            Brewery brewery = new Brewery(producer.getString(NAME), producer.getString(NOTES));
+            JSONArray produce = producer.getJSONArray(PRODUCE);
+            for(int produceIndex=0; produceIndex<produce.length(); produceIndex++){
+                JSONObject product = produce.getJSONObject(produceIndex);
+                Beer beer = new Beer(brewery,
+                                product.getString(NAME),
+                                (float)product.getDouble(ABV),
+                                product.getString(NOTES));
+                
+                beers.add(beer);
             }
+            
         }
-
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
-        private static Beer createBeer(final String breweryName, final String breweryNotes,
-                                       final String beerName, final String abvString, final String beerNotes) {
-            float abv;
-            try {
-                abv = Float.parseFloat(abvString);
-            } catch (NumberFormatException nfx) {
-                abv = Float.NaN;
-            }
-            Brewery brewery = new Brewery(breweryName, breweryNotes);
-            Beer beer = new Beer(brewery, beerName, abv, beerNotes);
-            Log.d(TAG, "Loaded " + beer);
-            return beer;
-        }
-
+        return beers;
     }
 
 
-    private static JSONArray loadJson(InputStream inputStream) throws IOException, JSONException {
+    private static JSONObject loadJson(InputStream inputStream) throws IOException, JSONException {
         Log.i(TAG, "Loading beer list from input stream.");
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         StringBuilder builder = new StringBuilder();
@@ -102,7 +77,6 @@ public class JsonBeerList implements Iterable<Beer> {
             line = reader.readLine();
         }
         String jsonString = builder.toString();
-        JSONArray jsonArray = new JSONArray(jsonString);
-        return jsonArray;
+        return new JSONObject(jsonString);
     }
 }
