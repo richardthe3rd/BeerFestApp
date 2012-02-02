af067a4f4b9eb3b5b7b24608457c4028da95fc1e
Richard Alcock <richard.alcock@gmail.com>
2012-02-02 21:17:46 +0000

    Use JSON format that has array of producers contain array of products.

--- tests/src/ralcock/cbf/model/JsonBeerListTest.java
+++ tests/src/ralcock/cbf/model/JsonBeerListTest.java
@@ -3,16 +3,35 @@ package ralcock.cbf.model;
 import android.test.AndroidTestCase;
 
 import java.io.InputStream;
+import java.util.List;
+import java.util.Vector;
 
 public class JsonBeerListTest extends AndroidTestCase {
 
     public void testLoadBeers() throws Exception {
-        InputStream inputStream = BeerDatabaseTest.class.getResourceAsStream("resources/one_beer.txt");
-        final Beer expectedBeer = new Beer(new Brewery("BREWERY_NAME", "BREWERY_NOTES"), "BEER_NAME", 1.0f, "BEER_NOTES");
+        InputStream inputStream = BeerDatabaseTest.class.getResourceAsStream("resources/one_beer.json");
+        final Beer expectedBeer = new Beer(new Brewery("BREWERY_ONE", "BREWERY_ONE_NOTES"), "BEER_ONE", 1.0f, "BEER_ONE_NOTES");
         JsonBeerList jsonBeerList = new JsonBeerList(inputStream);
         for(Beer beer : jsonBeerList) {
             assertEquals(expectedBeer, beer);
         }
+    }
+
+    public void testLoadBeers2() throws Exception {
+        InputStream inputStream = BeerDatabaseTest.class.getResourceAsStream("resources/two_breweries_three_beers.json");
+
+        List<Beer> expectedBeers = new Vector<Beer>(3);
+        expectedBeers.add(new Beer(new Brewery("BREWERY_ONE", "BREWERY_ONE_NOTES"), "BEER_ONE",   1.1f, "BEER_ONE_NOTES"));
+        expectedBeers.add(new Beer(new Brewery("BREWERY_TWO", "BREWERY_TWO_NOTES"), "BEER_TWO",   2.2f, "BEER_TWO_NOTES"));
+        expectedBeers.add(new Beer(new Brewery("BREWERY_TWO", "BREWERY_TWO_NOTES"), "BEER_THREE", 3.3f, "BEER_THREE_NOTES"));
+
+        JsonBeerList jsonBeerList = new JsonBeerList(inputStream);
+        int index = 0;
+        for(Beer beer : jsonBeerList) {
+            assertEquals(expectedBeers.get(index++), beer);
+        }
 
     }
+
+
 }
