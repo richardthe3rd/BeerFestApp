7158320b622600e01ce13f36405f7caf1020d7f9
Richard Alcock <richard.alcock@gmail.com>
2012-11-04 12:00:22 +0000

    delete unused files

--- src/ralcock/cbf/LoadTaskListener.java
+++
@@ -1,9 +0,0 @@
-package ralcock.cbf;
-
-interface LoadTaskListener {
-    void notifyLoadTaskStarted();
-
-    void notifyLoadTaskComplete(final LoadBeersTask.Result result);
-
-    void notifyLoadTaskUpdate(String[] values);
-}
